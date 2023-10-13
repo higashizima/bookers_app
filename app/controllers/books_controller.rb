97d34412461c0b076258cc86a64651cff96ba790
Mika Higashizima <144198758+higashizima@users.noreply.github.com>
2023-10-13 04:53:13 +0000

    Initial Commit2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,33 @@
 class BooksController < ApplicationController
-  def index
+
+  def new
+    @book = Book.new
+  end
+  
+    def index
+    @book = Book.new
+    @books = Book.all 
+  end
+  
+  def create
+    #データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    book.save
+    redirect_to 'books/:id'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  
+  
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+  get 'books/index' =>'books#index'
+  post 'books/index' =>'books#create'
+  get 'books/:id' => 'books#show'
   get 'books/edit'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
